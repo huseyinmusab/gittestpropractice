88c90d5620fab7f827abdad40c89d9816a0b3573
huseyinmusab <huseyinmkocal@gmail.com>
2021-08-15 18:13:58 +0200

    first commit from huseyin

--- src/test/java/test1/ClassOne.java
+++ src/test/java/test1/ClassOne.java
@@ -2,7 +2,9 @@ package test1;
 
 public class ClassOne {
     public static void main(String[] args) {
+        System.out.println("Here is master branch");
         System.out.println("Hello World");
+        System.out.println("Hello today");
     }
 
 }
