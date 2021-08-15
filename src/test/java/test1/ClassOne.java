0e25c10f0b749a5e50d7c2cb5a8b214fec1aae0c
huseyinmusab <huseyinmkocal@gmail.com>
2021-08-15 18:22:21 +0200

    5th commit from local master

--- src/test/java/test1/ClassOne.java
+++ src/test/java/test1/ClassOne.java
@@ -3,6 +3,7 @@ package test1;
 public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Here is a new update on local master");
     }
 
 }
